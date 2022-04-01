fc1b55fe19a9916ad68d3a4374e54d96ad4301c1
Rumman <rumman.waqar@copperstonetech.com>
2022-04-01 10:20:19 -0600

    added temporal difference

--- vision-processor/src/main.rs
+++ vision-processor/src/main.rs
@@ -1,3 +1,94 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::core::absdiff;
+use opencv::highgui::wait_key;
+use opencv::imgcodecs::imread;
+use opencv::ximgproc::threshold;
+use opencv::{highgui, imgcodecs, imgproc, prelude::*, videoio, Result};
+use std::env;
+
+fn main() -> Result<()> {
+    let path = env::current_dir()
+        .unwrap()
+        .join("data")
+        .join("office")
+        .join("input");
+
+    let image1 = imread(
+        path.join("in000001.jpg").to_str().unwrap(),
+        imgcodecs::IMREAD_GRAYSCALE,
+    )?;
+    let image2 = imread(
+        path.join("in000002.jpg").to_str().unwrap(),
+        imgcodecs::IMREAD_GRAYSCALE,
+    )?;
+    let image3 = imread(
+        path.join("in000750.jpg").to_str().unwrap(),
+        imgcodecs::IMREAD_GRAYSCALE,
+    )?;
+    highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
+
+    let mut temporal_difference = vision_processor::temporal_difference::TemporalDifference::new();
+    let settings = vision_processor::temporal_difference::Settings {
+        blur_size: 5,
+        thresh: 60.0
+    };
+    let output = temporal_difference.process(&image1, settings)?;
+    if output.is_none() {
+        println!("first image good");
+    }
+    let output = temporal_difference.process(&image2, settings)?;
+    if output.is_none() {
+        println!("second image good");
+    }
+    let output = temporal_difference.process(&image3, settings)?;
+    if output.is_some() {
+        println!("got something for image 3");
+
+        highgui::imshow("window", &output.as_ref().unwrap())?;
+        wait_key(0)?;
+    }
+
+    // let image3 = imread(path.join("in000003.jpg").to_str().unwrap(),
+    //                     imgcodecs::IMREAD_GRAYSCALE)?;
+
+    // highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
+    // highgui::imshow("window", &image1)?;
+    //
+    // highgui::named_window("window1", highgui::WINDOW_FULLSCREEN)?;
+    // highgui::imshow("window1", &image2)?;
+    //
+    // let mut delta = Mat::default();
+    // absdiff(&image1, &image2, &mut delta)?;
+    //
+    // highgui::named_window("delta", highgui::WINDOW_FULLSCREEN)?;
+    // highgui::imshow("delta", &delta)?;
+    //
+    // highgui::named_window("threshold", highgui::WINDOW_FULLSCREEN)?;
+    // let mut threshold_mat = Mat::default();
+    // imgproc::threshold(
+    //     &delta,
+    //     &mut threshold_mat,
+    //     60.0,
+    //     255.0,
+    //     imgproc::THRESH_BINARY,
+    // )?;
+    //
+    // highgui::imshow("threshold", &threshold_mat)?;
+    // highgui::wait_key(0)?;
+
+    // let camera_id = 4;
+    // let mut cam = videoio::VideoCapture::new(camera_id, videoio::CAP_ANY)?;
+
+    // let mut frame = Mat::default();
+    //
+    // loop {
+    //     cam.read(&mut frame)?;
+    //     highgui::imshow("window", &frame)?;
+    //
+    //     let key = highgui::wait_key(1)?;
+    //     if key == 113 {
+    //         break;
+    //     }
+    // }
+
+    Ok(())
 }
---
+++ vision-processor/src/temporal_difference.rs
@@ -0,0 +1,76 @@
+use opencv::core::Mat;
+use opencv::platform_types::size_t;
+use opencv::{prelude::*, *};
+
+pub struct TemporalDifference {
+    frame1: Option<Mat>,
+    frame2: Option<Mat>,
+}
+
+#[derive(Copy, Clone)]
+pub struct Settings {
+    pub blur_size: i32,
+    pub thresh: f64,
+}
+
+impl TemporalDifference {
+    pub fn new() -> TemporalDifference {
+        TemporalDifference {
+            frame1: None,
+            frame2: None,
+        }
+    }
+
+    pub fn process(
+        &mut self,
+        frame: &Mat,
+        settings: Settings,
+    ) -> opencv::Result<Option<Mat>> {
+        let mut output: Option<Mat> = None;
+
+        if self.frame1.is_some() && self.frame2.is_some() {
+            let d1 = frame_difference(frame, self.frame1.as_ref().unwrap())?;
+            let d2 =
+                frame_difference(self.frame1.as_ref().unwrap(), self.frame2.as_ref().unwrap())?;
+
+            let mut processing_frame = bitwise_or(&d1, &d2)?;
+            processing_frame = median_blur(&processing_frame, settings.blur_size)?;
+            processing_frame = binary_threshold(&processing_frame, settings.thresh, 255.)?;
+            output = Some(processing_frame);
+        }
+        self.frame2 = self.frame1.clone();
+        self.frame1 = Some(frame.clone());
+
+        Ok(output)
+    }
+}
+
+fn frame_difference(x1: &Mat, x2: &Mat) -> Result<Mat> {
+    let mut output = Mat::default();
+    opencv::core::absdiff(x1, x2, &mut output)?;
+    Ok(output)
+}
+
+fn bitwise_or(x1: &Mat, x2: &Mat) -> Result<Mat> {
+    let mut output = Mat::default();
+    opencv::core::bitwise_or(x1, x2, &mut output, &Mat::default())?;
+    Ok(output)
+}
+
+fn median_blur(x: &Mat, kernel_size: i32) -> Result<Mat> {
+    let mut output = Mat::default();
+    opencv::imgproc::median_blur(x, &mut output, kernel_size)?;
+    Ok(output)
+}
+
+fn binary_threshold(x: &Mat, thresh: f64, max_val: f64) -> Result<Mat> {
+    let mut output = Mat::default();
+    opencv::imgproc::threshold(
+        x,
+        &mut output,
+        thresh,
+        max_val,
+        opencv::imgproc::THRESH_BINARY,
+    )?;
+    Ok(output)
+}
